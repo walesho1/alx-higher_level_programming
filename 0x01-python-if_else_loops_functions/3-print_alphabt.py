b79e281d9bb3d59772e2518e9353a20b1d5c0743
olawale <waleshogbaike@yahoo.com>
2022-11-29 11:09:40 -0800

    task 03 update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 26):
-    if i == 4 or i == 16:
-        continue
-    else:
-        print("{:s}".format(chr(ord('a') + i),), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
