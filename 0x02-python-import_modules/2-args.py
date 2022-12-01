3e4b9b424f3488b0314b7bfcd6aacbbb6f57921c
olawale <waleshogbaike@yahoo.com>
2022-12-01 03:12:00 -0800

    task 2 update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
-        for i in range(1, num):
-            print("{}: {}".format(i, sys.argv[i]))
+if __name__ == '__main__':
+    from sys import argv
+nargs = len(argv)
+if nargs == 1:
+    print("{:d} arguments.".format(nargs - 1))
+elif nargs > 1:
+    message = "argument" if (nargs == 2) else "arguments"
+    print("{:d} {:s}:".format(nargs - 1, message))
+    for i in range(1, nargs):
+        print("{:d}: {:s}".format(i, argv[i]))
