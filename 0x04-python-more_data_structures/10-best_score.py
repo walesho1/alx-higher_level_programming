3385a5388ede61b565cd32665c2042f2772fc61c
olawale <waleshogbaike@yahoo.com>
2022-12-07 08:10:21 -0800

    task 10 update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary)
+    if a_dictionary is not None:
+        best_score = 0
+        for score in a_dictionary.values():
+            if best_score < score:
+                best_score = score
+        for name, score in a_dictionary.items():
+            if score == best_score:
+                return name
+    else:
+        return
