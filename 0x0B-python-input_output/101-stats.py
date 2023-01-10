f251d62401911fd6c4cc2591a061873cb98deb27
olawale <waleshogbaike@yahoo.com>
2023-01-10 07:46:45 -0800

    task 13 update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
 """
-Script reads stdin line by line and computes metrics
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-Each 10 lines and after a keyboard interruption (CTRL + C),
-prints those statistics since the beginning:
-total file size and
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-format: File size: <total size>
-format: <status code (in ascending order)>: <number>
+reads stdin line by line and computes metrics
 """
-
-
 import sys
 
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
+try:
+    for line in sys.stdin:
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-def print_size_and_codes(size, stat_codes):
-    print("File size: {:d}".format(size))
-    for k, v in sorted(stat_codes.items()):
-        if v:
-            print("{:s}: {:d}".format(k, v))
-
-
-def parse_stdin_and_compute():
-    size = 0
-    lines = 0
-    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                  "403": 0, "404": 0, "405": 0, "500": 0}
-    try:
-        for line in sys.stdin:
-            fields = list(map(str, line.strip().split(" ")))
-            size += int(fields[-1])
-            if fields[-2] in stat_codes:
-                stat_codes[fields[-2]] += 1
-            lines += 1
-            if lines % 10 == 0:
-                print_size_and_codes(size, stat_codes)
-    except KeyboardInterrupt:
-        print_size_and_codes(size, stat_codes)
-        raise
-
-    print_size_and_codes(size, stat_codes)
-
-
-parse_stdin_and_compute()
+except KeyboardInterrupt:
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
