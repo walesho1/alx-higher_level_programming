7ad9a551ecb0b12dea85464ef0a4f06250e65622
olawale <waleshogbaike@yahoo.com>
2023-01-16 13:31:27 -0800

    task update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,235 +1,117 @@
 #!/usr/bin/python3
+"""Module containing the ``Base`` class definition.
 """
-This module contains the "Base" class
-"""
-
-import csv
 import json
-import turtle
+import csv
+import os.path
 
 
 class Base:
-    """A base class"""
+    """``Base`` class definition.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize the base class"""
-        if id is None:
-            Base.__nb_objects += 1
-            self.id = self.__nb_objects
-        else:
+        """Initializes objects of class ``Base``.
+        """
+        if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of a list of dictionaries"""
+        """returns the json string representation of a list of dictionaries.
+        """
         if list_dictionaries is None:
-            list_dictionaries = []
-        return json.dumps(list_dictionaries)
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
+        """returns the actual object represented by the json string.
+        """
+        if json_string is None:
             return []
-        return json.loads(json_string)
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs to a file"""
-        filename = cls.__name__ + ".json"
-        lo = []
-        if list_objs is not None:
-            for i in list_objs:
-                lo.append(cls.to_dictionary(i))
-        with open(filename, 'w') as f:
-            f.write(cls.to_json_string(lo))
+        """saves list of objects to files.
+        """
+        if list_objs is None:
+            objs = "[]"
+        else:
+            obj_dicts = []
+            for obj in list_objs:
+                obj_dict = obj.to_dictionary()
+                obj_dicts.append(obj_dict)
+            objs = Base.to_json_string(obj_dicts)
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(objs)
 
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
-        if cls.__name__ is "Rectangle":
-            dummy = cls(1, 1)
-        elif cls.__name__ is "Square":
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
+        """creates a new object from the values in dictionary.
+        """
+        obj = cls(1, 1)
+        obj.update(**dictionary)
+        return obj
 
     @classmethod
     def load_from_file(cls):
-        filename = cls.__name__ + ".json"
-        l = []
-        try:
-            with open(filename, 'r') as f:
-                l = cls.from_json_string(f.read())
-            for i, e in enumerate(l):
-                l[i] = cls.create(**l[i])
-        except:
-            pass
-        return l
+        """Creates a new instance of the class from the contents of a file.
+        """
+        filename = "{}.json".format(cls.__name__)
+        list_objs = []
+        if not os.path.exists(filename):
+            return []
+        else:
+            with open(filename, 'r', encoding='utf-8') as file:
+                json_string = file.read()
+            list_dictionaries = Base.from_json_string(json_string)
+            for dictionary in list_dictionaries:
+                obj = cls.create(**dictionary)
+                list_objs.append(obj)
+            return list_objs
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """serializes a list of Rectangles/Squares in csv"""
-        filename = cls.__name__ + ".csv"
-        with open(filename, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            if cls.__name__ is "Rectangle":
-                for obj in list_objs:
-                    csv_writer.writerow([obj.id, obj.width, obj.height,
-                                         obj.x, obj.y])
-            elif cls.__name__ is "Square":
-                for obj in list_objs:
-                    csv_writer.writerow([obj.id, obj.size, obj.x, obj.y])
+        """saves the data of a list (Serialization) of objects to the a CSV
+        file.
+        """
+        class_name = cls.__name__
+        filename = "{}.csv".format(class_name)
+        if class_name == 'Rectangle':
+            fields = ['id', 'width', 'height', 'x', 'y']
+        elif class_name == 'Square':
+            fields = ['id', 'size', 'x', 'y']
+        with open(filename, 'w', encoding='utf-8') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fields)
+            for obj in list_objs:
+                writer.writerow(obj.to_dictionary())
 
     @classmethod
     def load_from_file_csv(cls):
-        """deserializes a list of Rectangles/Squares in csv"""
-        filename = cls.__name__ + ".csv"
-        l = []
-        try:
-            with open(filename, 'r') as csvfile:
-                csv_reader = csv.reader(csvfile)
-                for args in csv_reader:
-                    if cls.__name__ is "Rectangle":
-                        dictionary = {"id": int(args[0]),
-                                      "width": int(args[1]),
-                                      "height": int(args[2]),
-                                      "x": int(args[3]),
-                                      "y": int(args[4])}
-                    elif cls.__name__ is "Square":
-                        dictionary = {"id": int(args[0]), "size": int(args[1]),
-                                      "x": int(args[2]), "y": int(args[3])}
-                    obj = cls.create(**dictionary)
-                    l.append(obj)
-        except:
-            pass
-        return l
-
-    @staticmethod
-    def draw(list_rectangles, list_squares):
-        """opens a window and draws all the Rectangles and Squares"""
-        screen_width = 620
-        padding = 10
-        row_width = padding
-        row_height = 0
-        screen_height = padding
-        color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo',
-                      'violet']
-        color_size = len(color_list)
-        color_index = 0
-        for rect in list_rectangles:
-            potential_width = row_width + rect.width + rect.x + padding
-            if (row_width == padding or potential_width < screen_width):
-                row_width += rect.width + rect.x + padding
-                if (row_height < rect.height + rect.y):
-                    row_height = rect.height + rect.y
-            else:
-                screen_height += row_height + padding
-                row_width = rect.width + rect.x + padding * 2
-                row_height = rect.height + rect.y
-
-        for square in list_squares:
-            potential_width = row_width + square.size + square.x + padding
-            if (row_width == padding or potential_width < screen_width):
-                row_width += square.size + square.x + padding
-                if (row_height < square.size + square.y):
-                    row_height = square.size + square.y
-            else:
-                screen_height += row_height + padding
-                row_width = square.size + square.x + padding * 2
-                row_height = square.size + square.y
-        turtle.screensize(canvwidth=screen_width, canvheight=screen_height)
-        turtle.pu()
-        turtle.left(180)
-        turtle.forward(screen_width/2 - padding)
-        turtle.right(90)
-        turtle.forward(screen_height/2 - padding)
-        turtle.right(90)
-        row_width = padding
-        row_height = 0
-        for rect in list_rectangles:
-            potential_width = row_width + rect.width + rect.x + padding
-            if (row_width == padding or potential_width < screen_width):
-                row_width += rect.width + rect.x + padding
-                if (row_height < rect.height + rect.y):
-                    row_height = rect.height + rect.y
-            else:
-                turtle.pu()
-                turtle.left(180)
-                turtle.forward(row_width - padding)
-                turtle.left(90)
-                turtle.forward(row_height + padding)
-                turtle.left(90)
-                row_width = rect.width + rect.x + padding * 2
-                row_height = rect.height + rect.y
-            turtle.pd()
-            turtle.pencolor(color_list[color_index % color_size])
-            for _ in range(4):
-                turtle.forward(5)
-                turtle.back(5)
-                turtle.right(90)
-            turtle.pu()
-            turtle.forward(rect.x)
-            turtle.right(90)
-            turtle.forward(rect.y)
-            turtle.left(90)
-            turtle.pd()
-            turtle.pencolor('black')
-            turtle.fillcolor(color_list[color_index % color_size])
-            turtle.begin_fill()
-            for _ in range(2):
-                turtle.forward(rect.width)
-                turtle.right(90)
-                turtle.forward(rect.height)
-                turtle.right(90)
-            turtle.end_fill()
-            color_index += 1
-            turtle.pu()
-            turtle.forward(rect.width + padding)
-            turtle.left(90)
-            turtle.forward(rect.y)
-            turtle.right(90)
-
-        for square in list_squares:
-            potential_width = row_width + square.size + square.x + padding
-            if (row_width == padding or potential_width < screen_width):
-                row_width += square.size + square.x + padding
-                if (row_height < square.size):
-                    row_height = square.size + square.y
-            else:
-                turtle.pu()
-                turtle.left(180)
-                turtle.forward(row_width - padding)
-                turtle.left(90)
-                turtle.forward(row_height + padding)
-                turtle.left(90)
-                row_width = square.size + square.x + padding * 2
-                row_height = square.size + square.y
-            turtle.pd()
-            turtle.pencolor(color_list[color_index % color_size])
-            for _ in range(4):
-                turtle.forward(5)
-                turtle.back(5)
-                turtle.right(90)
-            turtle.pu()
-            turtle.forward(square.x)
-            turtle.right(90)
-            turtle.forward(square.y)
-            turtle.left(90)
-            turtle.pd()
-            turtle.pencolor('black')
-            turtle.fillcolor(color_list[color_index % color_size])
-            turtle.begin_fill()
-            for _ in range(4):
-                turtle.forward(square.size)
-                turtle.right(90)
-            turtle.end_fill()
-            color_index += 1
-            turtle.pu()
-            turtle.forward(square.size + padding)
-            turtle.left(90)
-            turtle.forward(square.y)
-            turtle.right(90)
-
-        turtle.getscreen()._root.mainloop()
+        """retrieves objects from a CSV file.
+        """
+        class_name = cls.__name__
+        filename = "{}.csv".format(class_name)
+        objs = []
+        if class_name == 'Rectangle':
+            fields = ['id', 'width', 'height', 'x', 'y']
+        elif class_name == 'Square':
+            fields = ['id', 'size', 'x', 'y']
+        with open(filename, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file, fieldnames=fields)
+            for row in csv_reader:
+                attrs = {}
+                for key, value in row.items():
+                    attrs[key] = int(value)
+                obj = cls.create(**attrs)
+                objs.append(obj)
+        return objs
