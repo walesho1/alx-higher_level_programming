d5dd771f969f6bf5cd7bae853fe19dcc67e256a8
olawale <waleshogbaike@yahoo.com>
2023-03-30 08:50:21 -0700

    task update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
+# display all HTTP methods the server will accept using curl.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
