731044bbfcef1a78a89c3de156ca2c2c1398ceaf
olawale <waleshogbaike@yahoo.com>
2023-03-30 08:08:02 -0700

    task update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# Script that takes in a URL, sends a request and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
