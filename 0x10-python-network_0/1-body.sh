5f393d759522f5fa6612625a0feed689061715ee
olawale <waleshogbaike@yahoo.com>
2023-03-30 08:05:55 -0700

    task update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
+# This script takes in a URL, sends GET request and displays body of response
 curl -sL "$1"
